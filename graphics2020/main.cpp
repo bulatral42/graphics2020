40322e760ac7892eee335a4b1ee2de8e3a6e9b3d
HP <HP@LAPTOP-89JVAFEG>
2020-11-23 10:37:45 +0300

    Coloured window, key_callback

--- graphics2020/main.cpp
+++ graphics2020/main.cpp
@@ -5,8 +5,55 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+
+void key_callback(GLFWwindow* window, int key,
+	int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	// glfw init end
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "TrialWindow", 
+		                                  nullptr, nullptr);
+	if (window == nullptr) {
+		std::cout << "Window creation error" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cout << "GLEW init error" << std::endl;
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	glViewport(0, 0, width, height);
+
+	glfwSetKeyCallback(window, key_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		// отрисовки
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// отрисовки
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
