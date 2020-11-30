4ccf4893b3cc9799c3040a3aaa378bf395bcf611
HP <HP@LAPTOP-89JVAFEG>
2020-11-30 10:05:23 +0300

    Wood box

--- graphics2020/main.cpp
+++ graphics2020/main.cpp
@@ -3,7 +3,7 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
+#include <SOIL/SOIL.h>
 #include "Shaders/shader.h"
 
 
@@ -27,8 +27,8 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	// Window
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "TrialWindow", 
-		                                  nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, 
+		    "TexturedRectangle", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "Window creation error" << std::endl;
 		glfwTerminate();
@@ -49,92 +49,103 @@ int main()
 	glfwSetKeyCallback(window, key_callback);
 
 	// Shaders
-	Shader ourShaderGrad("C:/Users/HP/source/repos/graphics2020/LibStuff/Include/Shaders/src/vshader1.vsh", "C:/Users/HP/source/repos/graphics2020/LibStuff/Include/Shaders/src/fshader1.frag");
-	Shader ourShaderChange("C:/Users/HP/source/repos/graphics2020/LibStuff/Include/Shaders/src/vshader2.vsh", "C:/Users/HP/source/repos/graphics2020/LibStuff/Include/Shaders/src/fshader2.frag");
+	Shader ourShaderGrad("../LibStuff/Include/Shaders/src/vshader1.vsh", "../LibStuff/Include/Shaders/src/fshader1.frag");
+	//Shader ourShaderChange("../LibStuff/Include/Shaders/src/vshader2.vsh", "../LibStuff/Include/Shaders/src/fshader2.frag");
 
 
 	// Vertices
-	GLfloat vertTriangle[] = {
-	    -0.5f,  0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
-	     0.0f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-	    -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f
-	};
-
 	GLfloat vertRectangle[] = {
-	    0.5f, 0.5f, 0.0f,  // Верхний правый угол
-	    0.5f, 0.0f, 0.0f,  // Нижний правый угол
-	    0.0f, 0.0f, 0.0f,  // Нижний левый угол
-	    0.0f, 0.5f, 0.0f   // Верхний левый угол
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Верхний правый
+	    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Нижний правый
+	   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Нижний левый
+	   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Верхний левый
 	};
 
-	GLuint indices[] = {  // Помните, что мы начинаем с 0!
-	    0, 1, 3,   // Первый треугольник
-	    1, 2, 3    // Второй треугольник
+	GLuint indices[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
 
-	GLuint VBO[2], VAO[2];
-	glGenVertexArrays(2, VAO);
-	glGenBuffers(2, VBO);
+	GLfloat textColors[] = {
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		0.5f, 1.0f
+	};
 
-	glBindVertexArray(VAO[0]);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertTriangle),
-		vertTriangle, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
-            6 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
-		    6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-	glBindVertexArray(0);
-
-	GLuint EBO;
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
-	glBindVertexArray(VAO[1]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertRectangle),
 		vertRectangle, GL_STATIC_DRAW);
-	
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),
 		indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
-		3 * sizeof(GLfloat), (GLvoid*)0);
-
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
+            8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
+		    8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE,
+		    8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 	glBindVertexArray(0);
 
-	// Run
+	
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int textWidth, textHeight;
+	unsigned char* image = SOIL_load_image("container.jpg", &textWidth, &textHeight, 0, SOIL_LOAD_RGB);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textWidth, textHeight,
+	    	0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Run
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		// Rendering
-		glClearColor(0.2f, 0.8f, 0.9f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		// Draw the first triangle using the data from our first VAO
+		glBindTexture(GL_TEXTURE_2D, texture);
 		ourShaderGrad.Use();
-		glBindVertexArray(VAO[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
-		ourShaderChange.Use();
+		/*ourShaderChange.Use();
 		GLfloat timeValue = glfwGetTime();
 		GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
-		glUniform4f(glGetUniformLocation(ourShaderChange.Program, "ourColor"), 0.0f, greenValue, 0.0f, 1.0f);		
-		glBindVertexArray(VAO[1]);
+		glUniform4f(glGetUniformLocation(ourShaderChange.Program, "ourColor"), 0.0f, greenValue, 0.0f, 1.0f);	
+		*/
+		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
 
-	glDeleteVertexArrays(2, VAO);
-	glDeleteBuffers(2, VBO);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return 0;
